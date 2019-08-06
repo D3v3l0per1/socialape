3fee333b8ac45cc94c14d7204f85304f3cc77dea
D3v3l0per1 <manuel.widmoser12@gmail.com>
2019-08-06 16:28:55 +0200

    Added Like & Unlike functionality

--- socialape-client/src/components/Scream.js
+++ socialape-client/src/components/Scream.js
@@ -3,12 +3,23 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import PropTypes from "prop-types";
+import MyButton from "../util/MyButton";
 
 // MUI Stuff
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+
+// Icons
+import ChatIcon from "@material-ui/icons/Chat";
+import FavoritIcon from "@material-ui/icons/Favorite";
+import FavoritBorderIcon from "@material-ui/icons/FavoriteBorder";
+
+// Redux Stuff
+import { connect } from "react-redux";
+import { likeScream, unlikeScream } from "../redux/actions/dataActions";
 
 const styles = {
   card: {
@@ -20,13 +31,29 @@ const styles = {
   },
   contnet: {
     padding: 25,
-    objectFit: 'cover'
+    objectFit: "cover"
   }
 };
 
 class Scream extends Component {
+  likedScream = () => {
+    if (
+      this.props.user.likes &&
+      this.props.user.likes.find(
+        like => like.screamId === this.props.scream.screamId
+      )
+    )
+      return true;
+    else return false;
+  };
+  likeScream = () => {
+    this.props.likeScream(this.props.scream.screamId);
+  };
+  unlikeScream = () => {
+    this.props.unlikeScream(this.props.scream.screamId);
+  };
   render() {
-    dayjs.extend(relativeTime)
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -37,8 +64,28 @@ class Scream extends Component {
         screamId,
         likeCount,
         commentCount
+      },
+      user: {
+        authenticated
       }
     } = this.props;
+    const likeButton = !authenticated ? (
+      <MyButton tip="Like">
+        <Link to="/login">
+          <FavoritBorderIcon color="primary" />
+        </Link>
+      </MyButton>
+    ) : (
+      this.likedScream() ? (
+        <MyButton tip="Unlike" onClick={this.unlikeScream}>
+          <FavoritIcon color="primary" />
+        </MyButton>
+      ) : (
+        <MyButton tip="Like" onClick={this.likeScream}>
+          <FavoritBorderIcon color="primary" />
+        </MyButton>
+      )
+    ) 
     return (
       <Card className={classes.card}>
         <CardMedia
@@ -56,13 +103,39 @@ class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            { dayjs(createdAt).fromNow() }
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
+          {likeButton}
+          <span>{likeCount} Likes</span>
+          <MyButton tip="comments">
+            <ChatIcon color="primary" />
+          </MyButton>
+          <span>{commentCount} Comments</span>
         </CardContent>
       </Card>
     );
   }
 }
 
-export default withStyles(styles)(Scream);
+Scream.propTypes = {
+  likeScream: PropTypes.func.isRequired,
+  unlikeScream: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  scream: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+const mapActionsToProps = {
+  likeScream,
+  unlikeScream
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Scream));
--- socialape-client/src/pages/home.js
+++ socialape-client/src/pages/home.js
@@ -1,30 +1,23 @@
 import React, { Component } from "react";
-import axios from "axios";
 import Grid from "@material-ui/core/Grid";
+import PropTypes from "prop-types";
 
-import Scream from '../components/Scream';
-import Profile from '../components/Profile'
+// Components
+import Scream from "../components/Scream";
+import Profile from "../components/Profile";
+
+// Redux Stuff
+import { connect } from "react-redux";
+import { getScreams } from "../redux/actions/dataActions";
 
 class home extends Component {
-  state = {
-    screams: null
-  };
   componentDidMount() {
-    axios
-      .get("/screams")
-      .then(res => {
-        // console.log(res.data);
-        this.setState({
-          screams: res.data
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    this.props.getScreams();
   }
   render() {
-    let recentScreamsMarkup = this.state.screams ? (
-      this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream}></Scream>)
+    const { screams, loading } = this.props.data;
+    let recentScreamsMarkup = !loading ? (
+      screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
     ) : (
       <p>Loading...</p>
     );
@@ -34,11 +27,23 @@ class home extends Component {
           {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <Profile></Profile>
+          <Profile />
         </Grid>
       </Grid>
     );
   }
 }
 
-export default home;
+home.propTypes = {
+  getScreams: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+export default connect(
+  mapStateToProps,
+  { getScreams }
+)(home);
